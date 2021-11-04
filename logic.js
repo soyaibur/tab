6ea82b08acdc19c9f155158e64c017ad1d7d9a2f
soyaibur <ovisoyaiburrahman@gmail.com>
2021-11-04 22:09:14 +0600

    completed js and End project

--- logic.js
+++ logic.js
@@ -4,11 +4,16 @@ const tabContents = document.querySelectorAll(".jakala");
 tabs.forEach(function tab(tab){
     tab.addEventListener('click',()=>{
         const target = document.querySelector(tab.dataset.tabTarget);
+
         tabContents.forEach(tabContent=>{
             tabContent.classList.remove('active');
         });
-        target.classList.add('active');
-        console.log(target.classList);
 
+        tabs.forEach(tab=>{
+            tab.classList.remove('active');
+        })
+        
+        tab.classList.add('active');
+        target.classList.add('active');
     });
 });
