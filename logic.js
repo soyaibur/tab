d1a61819de49c7c697fbf694a98f44d37e2ac4bb
soyaibur <ovisoyaiburrahman@gmail.com>
2021-11-05 06:51:15 +0600

    modify with comment

--- logic.js
+++ logic.js
@@ -1,19 +1,28 @@
-const tabs = document.querySelectorAll('[data-tab-target]');
-const tabContents = document.querySelectorAll(".jakala");
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('.jakala');
 
-tabs.forEach(function tab(tab){
-    tab.addEventListener('click',()=>{
-        const target = document.querySelector(tab.dataset.tabTarget);
 
-        tabContents.forEach(tabContent=>{
-            tabContent.classList.remove('active');
-        });
+//Setting switch to every tab element.
+tabs.forEach(tab=>{
+ tab.addEventListener('click',()=>{
+     const target = document.querySelector(tab.dataset.tabTarget);
 
-        tabs.forEach(tab=>{
-            tab.classList.remove('active');
-        })
-        
-        tab.classList.add('active');
-        target.classList.add('active');
-    });
+    
+     //removing active class from all div
+     tabContents.forEach(con=>{
+         con.classList.remove('active');
+     });
+    //removing active class from all tab
+    tabs.forEach(tab=>{
+        tab.classList.remove('active');
+    })
+
+
+
+
+    // Adding active class only to the clicked tab
+     tab.classList.add('active');
+     target.classList.add('active');
+     
+ })
 });
